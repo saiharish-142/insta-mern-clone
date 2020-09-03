8568cbce06e59e70de52f957cd5c95548ea36909
saiharish medam <61227933+saiharish-142@users.noreply.github.com>
2020-09-03 13:16:36 +0530

    build update

---
+++ client/build/precache-manifest.f55e1432f781c5021a83cb7d0097ad69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a3399774aae43e383a600622e5a9b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ea9cf5c4f9d154bcf3c",
+    "url": "/static/css/main.e06a0d6a.chunk.css"
+  },
+  {
+    "revision": "c99183e11cec44fe0962",
+    "url": "/static/js/2.d7261799.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.d7261799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ea9cf5c4f9d154bcf3c",
+    "url": "/static/js/main.147b9dbd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
